8f5c5c7742e56b9282f9e172082ed226ee81930b
M4xCod3 <maximiliano.arriapia@gmail.com>
2026-01-10 14:12:45 -0300

    Add main class to execute SimUrg

---
+++ Universidad/Java/lab4/main.java
@@ -0,0 +1,10 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Main {
+    public static void main(String[] args) {
+        SimUrg.SimUrg();
+    }
+}
